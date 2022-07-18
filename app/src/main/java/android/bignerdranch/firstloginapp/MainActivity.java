e58ae1535ba47802441f4afa5306b1464365a3d6
220114228 <220114228@tut4life.ac.za>
2022-07-18 15:26:09 +0200

    fourth commit

--- app/src/main/java/android/bignerdranch/firstloginapp/MainActivity.java
+++ app/src/main/java/android/bignerdranch/firstloginapp/MainActivity.java
@@ -3,13 +3,18 @@ package android.bignerdranch.firstloginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView)  findViewById(R.id.password);
+        CheckBox check = (CheckBox) findViewById(R.id.checkbox);
 
         MaterialButton loginBtn = (MaterialButton) findViewById(R.id.loginbtn);
 
@@ -34,6 +40,21 @@ public class MainActivity extends AppCompatActivity {
                        Toast.makeText(MainActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
 
                    }
+            }
+        });
+        //CheckBox. We use the compoundButton when the state changes to checked or not checked, the method below
+        //helps us to have a boolean as a parameter to verify the condition
+        check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+              if(isChecked){//The HideReturnsTransformationMethod returns any character to be hidden when displayingit
+                  //The TransformationMethod replaces characters with dots
+                      password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());//Display password
+
+
+              }else{//Hide password
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+              }
             }
         });
     }
