5eae4a52a9cd3b7cfd61b2be4195cb0201e4d7df
220114228 <220114228@tut4life.ac.za>
2022-07-20 03:20:16 +0200

    Sixth commint

--- app/src/main/java/android/bignerdranch/firstloginapp/UploadPicture.java
+++ app/src/main/java/android/bignerdranch/firstloginapp/UploadPicture.java
@@ -1,14 +1,65 @@
 package android.bignerdranch.firstloginapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class UploadPicture extends AppCompatActivity {
-
+      ImageView profilePic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_picture);
+        profilePic = findViewById(R.id.profile);
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                boolean choose = true;
+                if(choose == true){
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }else chooseImage();
+
+                    }else{
+                          if(!checkStoratePermissiom()){
+                              requestStoragePermission();
+                          }else chooseImage();
+
+                }
+                }
+            }
+        });
+    }
+
+    private boolean checkCameraPermission(){
+          boolean resource1= ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
+          boolean resource2= ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+
+         return resource1 && resource2;
+    }
+    private boolean checkStoratePermissiom(){
+        boolean resource2= ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+
+        return resource2;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestCameraPermission(){
+        requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestStoragePermission(){
+        requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+    }
+    private void  chooseImage(){
+
     }
 }
