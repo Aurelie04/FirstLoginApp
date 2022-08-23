56130f6d5fb77fe3b5c86f057eeee99cc8312cfe
220114228 <220114228@tut4life.ac.za>
2022-08-24 01:05:42 +0200

    ActivityResultLauncher

--- app/src/main/java/android/bignerdranch/firstloginapp/UploadPicture.java
+++ app/src/main/java/android/bignerdranch/firstloginapp/UploadPicture.java
@@ -1,97 +1,111 @@
 package android.bignerdranch.firstloginapp;
 
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 
 
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.security.Permission;
+
 
 //Add the following permissions in the manifest line 5--:
-//<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
-//<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
-//<uses-permission android:name="android.permission.CAMERA"/>
-//<uses-permission android:name="android.hardware.camera.autofocus"/>
+//External storage permission
 public class UploadPicture extends AppCompatActivity {
-    //private int REQUEST_CODE =1;
 
+    Button btnCamera;
+    ImageView profile;
+    ActivityResultLauncher<String> mGetContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Button btnCamera = (Button) findViewById(R.id.btnCamera);
-        ImageView profile = ( ImageView) findViewById(R.id.profile);
+        setContentView(R.layout.activity_main);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        profile = (ImageView) findViewById(R.id.profile);
 
-//        btnCamera.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                  if(ContextCompat.checkSelfPermission(UploadPicture.this,
-//                          Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
-//                      //Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                      //            startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
-//                      //Or display a toast
-//                      Toast.makeText(UploadPicture.this,"The permission has been granted",Toast.LENGTH_SHORT);
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                     profile.setImageURI(result);
+                    }
+                });
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContent.launch("image/*");
+            }
+        });
+
+//        btnCamera.setOnClickListener(this);
+//        handlePermission();
+//    }
 //
-//                  }else{
-//                      requestStoragePermission();
-//                  }
-//            }
-//        });
-
-
-
-
-    }
-
-//    private void requestStoragePermission() {
-//        //
-//        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
-//         new AlertDialog.Builder(this)
-//                 .setTitle("Reason")
-//                 .setMessage("The permission is needes to upload a profile picture for the application.")
-//                 .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-//                     @Override
-//                     public void onClick(DialogInterface dialogInterface, int i) {
-//                         ActivityCompat.requestPermissions(UploadPicture.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_CODE);
-//
-//                     }
-//                 })
-//                 .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-//                     @Override
-//                     public void onClick(DialogInterface dialogInterface, int i) {
-//                         dialogInterface.dismiss();
-//                     }
-//                 })
-//                 .create().show();
-//        }else{
-//            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_CODE);
+//    void handlePermission() {
+//        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+//            return;
+//        }
+//        if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_GRANTED)) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, SELECT_IMAGE);
 //        }
 //    }
-
+//
 //    @Override
 //    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if (requestCode == REQUEST_CODE) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                // Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                //startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
+//        switch(requestCode){
+//            case SELECT_IMAGE:
+//                for(int i = 0; i<permissions.length;i++) {
+//                    String permission = permissions[i];
+//                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+//                        boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,permission);
+//                    if(showRationale){
+//                        //Show your message
+//                    }else{
+//                        //User tapped never ask again
+//                        showSettingAlert();
+//                    }
+//                    }else{
+//                        // we are good
+//                    }
+//                }
 //
-//                Toast.makeText(this, "Permission grated", Toast.LENGTH_SHORT).show();
-//            } else {
-//                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
-//
-//            }
 //        }
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 //    }
+//
+//    private void showSettingAlert() {
+//
+//    }
+//    void openImageChooser(){
+//        Intent intent = new Intent();
+//        intent.setType("image/*");
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+//        startActivityR
+//    }
+//
+//    @Override
+//    public void onClick(View view) {
+//
+//    }
+    }
 }
+
